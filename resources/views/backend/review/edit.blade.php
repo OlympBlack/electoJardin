3b454c82df93794609ebdbf342533fe2545cbe86
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2025-05-21 13:23:20 +0200

    End of review management

--- resources/views/backend/review/edit.blade.php
+++ resources/views/backend/review/edit.blade.php
@@ -1,31 +1,31 @@
 @extends('backend.layouts.master')
 
-@section('title','Review Edit')
+@section('title','Modification de l\'avis')
 
 @section('main-content')
 <div class="card">
-  <h5 class="card-header">Review Edit</h5>
+  <h5 class="card-header">Modification de l'avis</h5>
   <div class="card-body">
     <form action="{{route('review.update',$review->id)}}" method="POST">
       @csrf
       @method('PATCH')
       <div class="form-group">
-        <label for="name">Review By:</label>
+        <label for="name">Avis de :</label>
         <input type="text" disabled class="form-control" value="{{$review->user_info->name}}">
       </div>
       <div class="form-group">
-        <label for="review">Review</label>
-      <textarea name="review" id="" cols="20" rows="10" class="form-control">{{$review->review}}</textarea>
+        <label for="review">Avis</label>
+        <textarea name="review" id="" cols="20" rows="10" class="form-control">{{$review->review}}</textarea>
       </div>
       <div class="form-group">
-        <label for="status">Status :</label>
+        <label for="status">Statut :</label>
         <select name="status" id="" class="form-control">
-          <option value="">--Select Status--</option>
-          <option value="active" {{(($review->status=='active')? 'selected' : '')}}>Active</option>
-          <option value="inactive" {{(($review->status=='inactive')? 'selected' : '')}}>Inactive</option>
+          <option value="">--Sélectionner le statut--</option>
+          <option value="active" {{(($review->status=='active')? 'selected' : '')}}>Actif</option>
+          <option value="inactive" {{(($review->status=='inactive')? 'selected' : '')}}>Inactif</option>
         </select>
       </div>
-      <button type="submit" class="btn btn-primary">Update</button>
+      <button type="submit" class="btn btn-primary">Mettre à jour</button>
     </form>
   </div>
 </div>
