4de4fd50cb66b3d98f12285085ff0cf3b4eb576a
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2025-05-21 14:14:26 +0200

    End of notification

--- resources/views/backend/notification/index.blade.php
+++ resources/views/backend/notification/index.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.layouts.master')
-@section('title','E-SHOP || All Notifications')
+@section('title','E-SHOP || Toutes les notifications')
 @section('main-content')
 <div class="card">
     <div class="row">
@@ -9,29 +9,29 @@
     </div>
   <h5 class="card-header">Notifications</h5>
   <div class="card-body">
-    @if(count(Auth::user()->Notifications)>0)
-    <table class="table  table-hover admin-table" id="notification-dataTable">
+    @if(count(Auth::user()->Notifications) > 0)
+    <table class="table table-hover admin-table" id="notification-dataTable">
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">Time</th>
-          <th scope="col">Title</th>
+          <th scope="col">Heure</th>
+          <th scope="col">Titre</th>
           <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
-        @foreach ( Auth::user()->Notifications as $notification)
+        @foreach (Auth::user()->Notifications as $notification)
 
         <tr class="@if($notification->unread()) bg-light border-left-light @else border-left-success @endif">
           <td scope="row">{{$loop->index +1}}</td>
-          <td>{{$notification->created_at->format('F d, Y h:i A')}}</td>
+          <td>{{$notification->created_at->format('d F Y h:i A')}}</td>
           <td>{{$notification->data['title']}}</td>
           <td>
-            <a href="{{route('admin.notification', $notification->id) }}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="view" data-placement="bottom"><i class="fas fa-eye"></i></a>
+            <a href="{{route('admin.notification', $notification->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px; border-radius:50%" data-toggle="tooltip" title="Voir" data-placement="bottom"><i class="fas fa-eye"></i></a>
             <form method="POST" action="{{ route('notification.delete', $notification->id) }}">
               @csrf
               @method('delete')
-                  <button class="btn btn-danger btn-sm dltBtn" data-id={{$notification->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
+              <button class="btn btn-danger btn-sm dltBtn" data-id={{$notification->id}} style="height:30px; width:30px; border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Supprimer"><i class="fas fa-trash-alt"></i></button>
             </form>
           </td>
         </tr>
@@ -40,39 +40,39 @@
       </tbody>
     </table>
     @else
-      <h2>Notifications Empty!</h2>
+      <h2>Pas de notifications !</h2>
     @endif
   </div>
 </div>
 @endsection
+
 @push('styles')
   <link href="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
-
 @endpush
+
 @push('scripts')
   <script src="{{asset('backend/vendor/datatables/jquery.dataTables.min.js')}}"></script>
   <script src="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-  <!-- Page level custom scripts -->
+  <!-- Scripts personnalisés pour la page -->
   <script src="{{asset('backend/js/demo/datatables-demo.js')}}"></script>
   <script>
+      $('#notification-dataTable').DataTable({
+          "columnDefs": [
+              {
+                  "orderable": false,
+                  "targets": [3]
+              }
+          ]
+      });
 
-      $('#notification-dataTable').DataTable( {
-            "columnDefs":[
-                {
-                    "orderable":false,
-                    "targets":[3]
-                }
-            ]
-        } );
+      // SweetAlert
 
-        // Sweet alert
-
-        function deleteData(id){
-
-        }
+      function deleteData(id){
+          // Fonction vide ici
+      }
   </script>
   <script>
     $(document).ready(function(){
@@ -81,26 +81,27 @@
               'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
           }
       });
-        $('.dltBtn').click(function(e){
-          var form=$(this).closest('form');
-            var dataID=$(this).data('id');
-            // alert(dataID);
-            e.preventDefault();
-            swal({
-                  title: "Are you sure?",
-                  text: "Once deleted, you will not be able to recover this data!",
-                  icon: "warning",
-                  buttons: true,
-                  dangerMode: true,
-              })
-              .then((willDelete) => {
-                  if (willDelete) {
-                    form.submit();
-                  } else {
-                      swal("Your data is safe!");
-                  }
-              });
-        })
-    })
+
+      $('.dltBtn').click(function(e){
+          var form = $(this).closest('form');
+          var dataID = $(this).data('id');
+          // alert(dataID);
+          e.preventDefault();
+          swal({
+              title: "Êtes-vous sûr ?",
+              text: "Une fois supprimées, vous ne pourrez plus récupérer ces données !",
+              icon: "warning",
+              buttons: true,
+              dangerMode: true,
+          })
+          .then((willDelete) => {
+              if (willDelete) {
+                  form.submit();
+              } else {
+                  swal("Vos données sont en sécurité !");
+              }
+          });
+      });
+    });
   </script>
 @endpush
