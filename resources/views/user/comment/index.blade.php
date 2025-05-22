7e318ff875b5351fbec15d040ad43f231f3f9fe9
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2025-05-22 13:00:13 +0200

    End of comment-users

--- resources/views/user/comment/index.blade.php
+++ resources/views/user/comment/index.blade.php
@@ -1,7 +1,7 @@
 @extends('user.layouts.master')
-@section('title','E-SHOP || Comment Page')
+@section('title','E-SHOP || Page des Commentaires')
 @section('main-content')
- <!-- DataTales Example -->
+ <!-- Exemple de DataTables -->
  <div class="card shadow mb-4">
      <div class="row">
          <div class="col-md-12">
@@ -9,7 +9,7 @@
          </div>
      </div>
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary float-left">Comment Lists</h6>
+      <h6 class="m-0 font-weight-bold text-primary float-left">Liste des Commentaires</h6>
     </div>
     <div class="card-body">
       <div class="table-responsive">
@@ -17,23 +17,23 @@
         <table class="table table-bordered" id="order-dataTable" width="100%" cellspacing="0">
           <thead>
             <tr>
-              <th>S.N.</th>
-              <th>Author</th>
-              <th>Post Title</th>
+              <th>N°</th>
+              <th>Auteur</th>
+              <th>Titre de l’article</th>
               <th>Message</th>
               <th>Date</th>
-              <th>Status</th>
+              <th>Statut</th>
               <th>Action</th>
             </tr>
           </thead>
           <tfoot>
             <tr>
-              <th>S.N.</th>
-              <th>Author</th>
-              <th>Post Title</th>
+              <th>N°</th>
+              <th>Auteur</th>
+              <th>Titre de l’article</th>
               <th>Message</th>
               <th>Date</th>
-              <th>Status</th>
+              <th>Statut</th>
               <th>Action</th>
             </tr>
           </tfoot>
@@ -44,7 +44,7 @@
                     <td>{{$comment->user_info['name']}}</td>
                     <td>{{$comment->post->title}}</td>
                     <td>{{$comment->comment}}</td>
-                    <td>{{$comment->created_at->format('M d D, Y g: i a')}}</td>
+                    <td>{{$comment->created_at->format('d M D, Y g:i a')}}</td>
                     <td>
                         @if($comment->status=='active')
                           <span class="badge badge-success">{{$comment->status}}</span>
@@ -53,11 +53,11 @@
                         @endif
                     </td>
                     <td>
-                        <a href="{{route('user.post-comment.edit',$comment->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                        <a href="{{route('user.post-comment.edit',$comment->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px; border-radius:50%" data-toggle="tooltip" title="Modifier" data-placement="bottom"><i class="fas fa-edit"></i></a>
                         <form method="POST" action="{{route('user.post-comment.delete',[$comment->id])}}">
                           @csrf
                           @method('delete')
-                              <button class="btn btn-danger btn-sm dltBtn" data-id={{$comment->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                              <button class="btn btn-danger btn-sm dltBtn" data-id={{$comment->id}} style="height:30px; width:30px; border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Supprimer"><i class="fas fa-trash-alt"></i></button>
                         </form>
                     </td>
                 </tr>
@@ -66,7 +66,7 @@
         </table>
         <span style="float:right">{{$comments->links()}}</span>
         @else
-          <h6 class="text-center">No post comments found!!!</h6>
+          <h6 class="text-center">Aucun commentaire trouvé !!!</h6>
         @endif
       </div>
     </div>
@@ -85,12 +85,12 @@
 
 @push('scripts')
 
-  <!-- Page level plugins -->
+  <!-- Plugins de niveau page -->
   <script src="{{asset('backend/vendor/datatables/jquery.dataTables.min.js')}}"></script>
   <script src="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-  <!-- Page level custom scripts -->
+  <!-- Scripts personnalisés de niveau page -->
   <script src="{{asset('backend/js/demo/datatables-demo.js')}}"></script>
   <script>
 
@@ -122,8 +122,8 @@
               // alert(dataID);
               e.preventDefault();
               swal({
-                    title: "Are you sure?",
-                    text: "Once deleted, you will not be able to recover this data!",
+                    title: "Êtes-vous sûr ?",
+                    text: "Une fois supprimées, vous ne pourrez plus récupérer ces données !",
                     icon: "warning",
                     buttons: true,
                     dangerMode: true,
@@ -132,7 +132,7 @@
                     if (willDelete) {
                        form.submit();
                     } else {
-                        swal("Your data is safe!");
+                        swal("Vos données sont en sécurité !");
                     }
                 });
           })
